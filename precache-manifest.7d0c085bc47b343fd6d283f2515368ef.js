d3133a6623f8856a0bb59ebee92dedb5b83c2ad0
Liucija <liuciij@gmail.com>
2020-10-11 10:33:13 +0100

    Updates

---
+++ precache-manifest.7d0c085bc47b343fd6d283f2515368ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110e9d551bbc2c0a7253f09875fa01f2",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "60dc4b68de55950585de",
+    "url": "/resume/static/css/main.9122533c.chunk.css"
+  },
+  {
+    "revision": "1cc452227de837966198",
+    "url": "/resume/static/js/2.9dcc7651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.9dcc7651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60dc4b68de55950585de",
+    "url": "/resume/static/js/main.ade54a7e.chunk.js"
+  },
+  {
+    "revision": "80b6511a4fe8944e422b",
+    "url": "/resume/static/js/runtime-main.3ebc219f.js"
+  }
+]);
